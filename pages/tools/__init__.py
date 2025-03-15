3cabc8fe45125be9c0537256ed179be9eda9c29c
AsusHP <iurigabriel2009@hotmail.com>
2025-03-15 11:44:45 -0300

    refatorar: mover arquivos de conexão e login para a nova estrutura de diretórios

---
+++ pages/tools/__init__.py
@@ -0,0 +1,4 @@
+from tools.classes import *
+from tools.login import *
+from tools.movimentacoes import *
+from tools.tipo_operacao import *
--- tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from .classes import *
-from .login import *
-from .movimentacoes import *
-from .tipo_operacao import *
-
